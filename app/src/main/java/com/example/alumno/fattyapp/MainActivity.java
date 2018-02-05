0b40f4e6f8862a942b3e268f3bfed2ad2422374d
Evildark666 <evildark41@gmail.com>
2018-02-05 13:32:57 +0100

    Pantalla de primera ejecucion explicativa hecha A TOPE

--- app/src/main/java/com/example/alumno/fattyapp/MainActivity.java
+++ app/src/main/java/com/example/alumno/fattyapp/MainActivity.java
@@ -15,21 +15,22 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_main);
         SharedPreferences settings=getSharedPreferences("prefs",0);
         boolean firstRun=settings.getBoolean("firstRun",false);
-        if(firstRun==false)//if running for first time
-        //Splash will load for first time
-        {
+
+
+
+        if(firstRun==false){//si se ejecuta por primera vez
             SharedPreferences.Editor editor=settings.edit();
             editor.putBoolean("firstRun",true);
             editor.commit();
             Intent i=new Intent(this,explicacion_app1.class);
             startActivity(i);
             finish();
-        }
 
-        else {
+        } else {
+
 
 
         }
